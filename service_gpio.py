83d19958d85149780eb6cc26b8f8e8c675a4f09f
Maxime Terras <maxime@ruggedpod.qyshare.com>
2015-02-24 21:30:16 +0000

    Add xml response

--- service_gpio.py
+++ service_gpio.py
@@ -15,10 +15,20 @@ def init():
 
 def SetBladeAttentionLEDOn( bladeId ):
     GPIO.output( ledTable[ bladeId ], True)
-    return 'OK'
+    response = etree.Element('BladeResponse')
+    etree.SubElement(response, 'CompletionCode').text = 'Success'
+    etree.SubElement(response, 'statusDescription').text = ''
+    etree.SubElement(response, 'apiVersion').text = '1'
+    etree.SubElement(response, 'bladeNumber').text = bladeId
+    return etree.tostring(response, pretty_print=True)
+
 
 def SetBladeAttentionLEDOff( bladeId ):
     GPIO.output( ledTable[ bladeId ], False)
-    return 'OK'
-
+    response = etree.Element('BladeResponse')
+    etree.SubElement(response, 'CompletionCode').text = 'Success'
+    etree.SubElement(response, 'statusDescription').text = ''
+    etree.SubElement(response, 'apiVersion').text = '1'
+    etree.SubElement(response, 'bladeNumber').text = bladeId
+    return etree.tostring(response, pretty_print=True)
 
