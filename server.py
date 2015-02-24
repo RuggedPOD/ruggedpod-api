cfc8d9a08d1425a186726d07eb2d09b6b6703c67
Maxime Terras <maxime@ruggedpod.qyshare.com>
2015-02-24 18:52:49 +0000

    Add dependency injection feature

--- server.py
+++ server.py
@@ -1,22 +1,19 @@
 from flask import Flask, request
-import RPi.GPIO as GPIO
-from lxml import etree
-
-
 app = Flask(__name__)
 
+mock=1
 
-# 
-ledTable={'1' : 7,
-          '2' : 12
-         }
+if mock == 1:
+    import service_mock as service
+else:
+    import service_gpio as service
 
 
 @app.route("/SetBladeAttentionLEDOn")
 def SetBladeAttentionLEDOn():
     if 'bladeId' in request.args:
-        GPIO.output(ledTable[request.args['bladeId']], True)
-        return 'OK'
+        data = service.SetBladeAttentionLEDOn( request.args['bladeId'] )
+        return data
     else:
         return 'Set bladeId'
 
@@ -24,31 +21,13 @@ def SetBladeAttentionLEDOn():
 @app.route("/SetBladeAttentionLEDOff")
 def SetBladeAttentionLEDOff():
     if 'bladeId' in request.args:
-        GPIO.output(ledTable[request.args['bladeId']], False)
+        service.SetBladeAttentionLEDOff( request.args['bladeId'] )
         return 'OK'
     else:
         return 'Set bladeId'
 
-@app.route("/SetAllBladesAttentionLEDOn")
-def SetAllBladesAttentionLEDOn():
-    for led in ledTable:
-        GPIO.output(ledTable[ led ], True)
-    return 'OK'
-
-@app.route("/SetAllBladesAttentionLEDOff")
-def SetAllBladesAttentionLEDOff():
-    for led in ledTable:
-        GPIO.output(ledTable[ led ], False)
-    return 'OK'
-
-
 if __name__ == "__main__":
-    GPIO.setwarnings(False)
-    GPIO.setmode(GPIO.BOARD)
-
-    # Set all led in Output
-    for led in ledTable:
-        GPIO.setup(ledTable[ led ], GPIO.OUT)
+    service.init()
 
     app.run(host= '0.0.0.0')
     app.run()
