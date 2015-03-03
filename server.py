9b0e8a4b67df3ba42e2ae816d6e2990c48026e5e
Pierre Padrixe <pierre.padrixe@numergy.com>
2015-03-03 18:17:28 +0100

    Add use of ParameterMissing exception which handle status code 400

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 import sys
 from flask import Flask, request
 
+from common import exception
 import service_gpio as service
 
 
@@ -9,10 +10,9 @@ app = Flask(__name__)
 
 @app.route("/SetBladeAttentionLEDOn")
 def set_blade_attention_led_on():
-    if 'bladeId' in request.args:
-        return service.set_blade_attention_led_on(request.args['bladeId'])
-    else:
-        return 'Set bladeId'
+    if not 'bladeId' in request.args:
+        raise exception.ParameterMissing(name="bladeId")
+    return service.set_blade_attention_led_on(request.args['bladeId'])
 
 @app.route("/SetAllBladesAttentionLEDOn")
 def set_all_blades_attention_led_on():
@@ -20,10 +20,9 @@ def set_all_blades_attention_led_on():
 
 @app.route("/SetBladeAttentionLEDOff")
 def set_blade_attention_led_off():
-    if 'bladeId' in request.args:
-        return service.set_blade_attention_led_off(request.args['bladeId'])
-    else:
-        return 'Set bladeId'
+    if not 'bladeId' in request.args:
+        raise exception.ParameterMissing(name="bladeId")
+    return service.set_blade_attention_led_off(request.args['bladeId'])
 
 @app.route("/SetAllBladesAttentionLEDOff")
 def set_all_blades_attention_led_off():
@@ -35,24 +34,21 @@ def get_all_power_state():
 
 @app.route("/GetPowerState")
 def get_power_state():
-    if 'bladeId' in request.args:
-        return service.get_power_state(request.args['bladeId'])
-    else:
-        return 'Set bladeId'
+    if not 'bladeId' in request.args:
+        raise exception.ParameterMissing(name="bladeId")
+    return service.get_power_state(request.args['bladeId'])
 
 @app.route("/SetPowerOn")
 def set_power_on():
-    if 'bladeId' in request.args:
-        return service.set_power_on(request.args['bladeId'])
-    else:
-        return 'Set bladeId'
+    if not 'bladeId' in request.args:
+        raise exception.ParameterMissing(name="bladeId")
+    return service.set_power_on(request.args['bladeId'])
 
 @app.route("/SetPowerOff")
 def set_power_off():
-    if 'bladeId' in request.args:
-        return service.set_power_off(request.args['bladeId'])
-    else:
-        return 'Set bladeId'
+    if not 'bladeId' in request.args:
+        raise exception.ParameterMissing(name="bladeId")
+    return service.set_power_off(request.args['bladeId'])
 
 @app.route("/SetAllPowerOn")
 def set_all_power_on():
@@ -64,10 +60,9 @@ def set_all_power_off():
 
 @app.route("/SetBladeShortOnOff")
 def set_blade_short_onoff():
-    if 'bladeId' in request.args:
-        return service.set_blade_short_onoff(request.args['bladeId'])
-    else:
-        return 'Set bladeId'
+    if not 'bladeId' in request.args:
+        raise exception.ParameterMissing(name="bladeId")
+    return service.set_blade_short_onoff(request.args['bladeId'])
 
 @app.route("/SetAllBladesShortOnOff")
 def set_all_blades_short_onoff():
@@ -75,10 +70,9 @@ def set_all_blades_short_onoff():
 
 @app.route("/SetBladeLongOnOff")
 def set_blade_long_onoff():
-    if 'bladeId' in request.args:
-        return service.set_blade_long_onoff(request.args['bladeId'])
-    else:
-        return 'Set bladeId'
+    if not 'bladeId' in request.args:
+        raise exception.ParameterMissing(name="bladeId")
+    return service.set_blade_long_onoff(request.args['bladeId'])
 
 @app.route("/SetAllBladesLongOnOff")
 def set_all_blades_long_onoff():
@@ -86,10 +80,13 @@ def set_all_blades_long_onoff():
 
 @app.route("/StartBladeSerialSession")
 def start_blade_serial_session():
-    if 'bladeId' in request.args:
-        return service.start_blade_serial_session(request.args['bladeId'])
-    else:
-        return 'Set bladeId'
+    if not 'bladeId' in request.args:
+        raise exception.ParameterMissing(name="bladeId")
+    return service.start_blade_serial_session(request.args['bladeId'])
+
+@app.errorhandler(exception.ParameterMissing)
+def attribute_missing_handler(error):
+    return error.message, error.status_code
 
 
 if __name__ == "__main__":
