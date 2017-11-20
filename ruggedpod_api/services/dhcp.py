RuggedPOD/ruggedpod-api

commit 735f7546bf2759f3f82c64f08e19997dd4084bed

Fix blade IP resolution
IP address resolution was working only when the DHCP server
was running within the RuggedPOD controller. Few weeks ago we
introduced the DHCP-Proxy mode to leverage situations where
DHCP is running elsewhere on the network. In this case, reading
the DHCP lease file on the RuggedPOD controller makes no sense.

Now in this case, we read the IP address from the ARP table. If the
IP is not found, we try a discovery using nmap to populate the
ARP table and then retry to find the MAC address in the table.

There is still a problem with this approach. If the network range
is too big, for instance a /16 CIDR, the discovery is very long
(several minutes). For now, we recommend to use /24 networks or
smaller.

This last problem will be addressed later.

ruggedpod_api/services/dhcp.py
@@ -122,3 +122,17 @@ def refresh():
 
     if (rc != 0):
         raise exception.RuggedpodException()
+
+
+def read_ip_address_from_lease(mac):
+    dhcp_lease_file = "/var/lib/misc/dnsmasq.leases"
+
+    if not os.path.isfile(dhcp_lease_file):
+        return None
+
+    with open(dhcp_lease_file) as f:
+        for line in f.readlines():
+            search = re.search('.* ([0-9a-f:]{17}) ([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+) .*', line)
+            if search.group(1) == mac:
+                return search.group(2)
+    return None

ruggedpod_api/api/v2_0/blades.py
@@ -28,7 +28,7 @@ from .blueprint import api
 
 from ruggedpod_api.api import utils
 from ruggedpod_api.common import exception
-from ruggedpod_api.services import gpio, i2c
+from ruggedpod_api.services import gpio, i2c, dhcp, network
 from ruggedpod_api.services.db import Database, Blade, db
 
 from flask import request
@@ -43,14 +43,14 @@ def get_blades():
     with session.begin():
         blades = []
         for b in session.query(Blade):
-            mac_address = _format_mac_address_standard(b.mac_address)
+            mac_address = network.format_mac_address_standard(b.mac_address)
             blades.append({
                 'id': b.id,
                 'name': b.name,
                 'description': b.description,
                 'building': b.building,
                 'mac_address': mac_address,
-                'ip_address': _read_ip_address(mac_address),
+                'ip_address': network.read_ip_address(mac_address),
                 'consumption': i2c.read_power_consumption(str(b.id))
             })
         return json.dumps(blades)
@@ -76,14 +76,14 @@ def get_blade(id):
     session = db.session()
     with session.begin():
         blade = _get_blade(id, session)
-        mac_address = _format_mac_address_standard(blade.mac_address)
+        mac_address = network.format_mac_address_standard(blade.mac_address)
         return json.dumps({
             'id': blade.id,
             'name': blade.name,
             'description': blade.description,
             'building': blade.building,
             'mac_address': mac_address,
-            'ip_address': _read_ip_address(mac_address),
+            'ip_address': network.read_ip_address(mac_address),
             'consumption': i2c.read_power_consumption(str(blade.id))
         })
 
@@ -105,7 +105,7 @@ def update_blade(id):
             blade.description = data['description']
 
         if 'mac_address' in data:
-            blade.mac_address = _normalize_mac_address(data['mac_address'])
+            blade.mac_address = network.normalize_mac_address(data['mac_address'])
 
     return get_blade(id)
 
@@ -160,7 +160,7 @@ def build_blade(id):
 
         pxe_tftp_dir = "/tftp/pxe/pxelinux.cfg"
         pwe_www_dir = "/var/www/pxe"
-        pxe_mac_address = _format_mac_address_pxe(blade.mac_address)
+        pxe_mac_address = network.format_mac_address_pxe(blade.mac_address)
 
         ip = _get_ip_address('eth0')
 
@@ -244,7 +244,7 @@ def cancel_build_blade(id):
 
         pxe_tftp_dir = "/tftp/pxe/pxelinux.cfg"
         pwe_www_dir = "/var/www/pxe"
-        pxe_mac_address = _format_mac_address_pxe(blade.mac_address)
+        pxe_mac_address = network.format_mac_address_pxe(blade.mac_address)
 
         os.remove("%s/%s" % (pxe_tftp_dir, pxe_mac_address))
         os.remove("%s/%s.seed" % (pwe_www_dir, pxe_mac_address))
@@ -265,36 +265,3 @@ def _get_blade(id, session):
 def _long_action():
     long = request.args.get('long')
     return long is not None and (long.lower() == 'true' or long == '')
-
-
-def _normalize_mac_address(mac):
-    return mac.encode("ascii").translate(None, ":- ").lower()
-
-
-def _format_mac_address_standard(mac):
-    if not mac:
-        return None
-    n = _normalize_mac_address(mac)
-    return ':'.join(n[i:i+2] for i in range(0, 12, 2))
-
-
-def _format_mac_address_pxe(mac):
-    n = _normalize_mac_address(mac)
-    return "01-%s" % '-'.join(n[i:i+2] for i in range(0, 12, 2))
-
-
-def _read_ip_address(mac):
-    if not mac:
-        return None
-
-    dhcp_lease_file = "/var/lib/misc/dnsmasq.leases"
-
-    if not os.path.isfile(dhcp_lease_file):
-        return None
-
-    with open(dhcp_lease_file) as f:
-        for line in f.readlines():
-            search = re.search('.* ([0-9a-f:]{17}) ([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+) .*', line)
-            if search.group(1) == mac:
-                return search.group(2)
-    return None

ruggedpod_api/services/network.py
@@ -1,0 +1,79 @@
+# RuggedPOD management API
+#
+# Copyright (C) 2016 Guillaume Giamarchi <guillaume.giamarchi@gmail.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+import re
+
+from ruggedpod_api.common import exception
+from ruggedpod_api.services import dhcp, utils
+
+
+def normalize_mac_address(mac):
+    return mac.encode("ascii").translate(None, ":- ").lower()
+
+
+def format_mac_address_standard(mac):
+    if not mac:
+        return None
+    n = normalize_mac_address(mac)
+    return ':'.join(n[i:i+2] for i in range(0, 12, 2))
+
+
+def format_mac_address_pxe(mac):
+    n = normalize_mac_address(mac)
+    return "01-%s" % '-'.join(n[i:i+2] for i in range(0, 12, 2))
+
+
+def read_ip_address(mac):
+    if not mac:
+        return None
+
+    dhcp_config = dhcp.read_config()
+
+    if dhcp_config['dhcp_mode'] == 'dhcp':
+        return dhcp.read_ip_address_from_lease(mac)
+
+    ip = _read_ip_address_from_arp_table(mac)
+
+    if ip:
+        return ip
+
+    _network_discovery(dhcp_config['dhcp_network'])
+
+    return _read_ip_address_from_arp_table(mac)
+
+
+def _network_discovery(net_cidr):
+    nmap_cmd = "nmap --min-hostgroup 256 --min-parallelism 256 -sn %s" % net_cidr
+    rc, stdout, stderr = utils.cmd(nmap_cmd)
+    if rc != 0:
+        raise exception.RuggedpodException()
+
+
+def _read_ip_address_from_arp_table(mac):
+    rc, stdout, stderr = utils.cmd("arp -an")
+
+    if rc != 0:
+        raise exception.RuggedpodException()
+
+    n_mac = format_mac_address_standard(mac)
+
+    for line in stdout.split('\n'):
+        search = re.search('.* \(([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+)\) .* ([0-9a-f:]{17}) .*', line)
+        if search and n_mac == search.group(2):
+            return search.group(1)
+
+    return None

commit aa24455bc6e87727b7db8a0a1a9e7e6c0e3d5f4b

DHCP Server was broken
Commit 735f7546 introduced a bug causing some API answer 500
error. This was caused by some missing imports after a code
refactoring

ruggedpod_api/services/dhcp.py
@@ -15,7 +15,8 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-
+import re
+import os
 import subprocess
 
 from jinja2 import Environment, PackageLoader